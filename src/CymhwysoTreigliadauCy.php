53d3ebfeecb1ee0b66c0b8cce14c86f727020433
Ian Low <ianlow28@gmail.com>
2022-08-19 16:34:46 +0100

    Ymrwymiad cychwyniedig

---
+++ src/CymhwysoTreigliadauCy.php
@@ -0,0 +1,32 @@
+<?php
+/* ==============================================
+TO DO:
+======
+- details
+-------------------------------------------------
+DONE:
+=====
+# Changelog
+## 1.0.0 - yyyy-mm-dd
+### Changed
+- details
+### Fixed
+- details
+### Added
+- details
+================================================= */
+namespace Ianl28\CymhwysoTreigliadauCy;
+class CymhwysoTreigliadauCy{
+
+  //-------------------------------
+  public function __construct(){
+    echo "This is called from CymhwysoTreigliadauCy->__construct()\n";
+  }//endfunc
+  //-------------------------------
+  public function testrun(){
+    echo "This is called from CymhwysoTreigliadauCy->testrun()\n";
+  }//endfunc
+  //-------------------------------
+
+}//endclass 
+?>
